fa19de66f94c2db2d422807db8367c416b05bbf2
Ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2024-10-17 23:27:25 +0700

    implement tab bar image and color

--- InstagramClone/InstagramClone/Controller/FeedController.swift
+++ InstagramClone/InstagramClone/Controller/FeedController.swift
@@ -13,6 +13,6 @@ class FeedController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.systemGray
+        view.backgroundColor = UIColor.white
     }
 }
--- InstagramClone/InstagramClone/Controller/MainTabController.swift
+++ InstagramClone/InstagramClone/Controller/MainTabController.swift
@@ -16,15 +16,26 @@ class MainTabController: UITabBarController{
         configureViewControllers()
     }
     
-    //MARK: COnfigure UI
-    
+    //MARK: Configure UI
     private func configureViewControllers(){
-        let feed = FeedController()
-        let search = SearchController()
-        let post = PostController()
-        let notification = NotificationController()
-        let profile = ProfileController()
+        
+        let feed = templateNavController(unSelectedImage: UIImage(systemName: "house")!, selectedImage: UIImage(systemName: "house.fill")!, viewController: FeedController())
+        let search = templateNavController(unSelectedImage: UIImage(systemName: "magnifyingglass.circle")!, selectedImage: UIImage(systemName: "magnifyingglass.circle.fill")!, viewController: SearchController())
+        let post = templateNavController(unSelectedImage: UIImage(systemName: "plus.app")!, selectedImage: UIImage(systemName: "plus.app.fill")!, viewController: PostController())
+        let notification = templateNavController(unSelectedImage: UIImage(systemName: "heart")!, selectedImage: UIImage(systemName: "heart.fill")!, viewController: NotificationController())
+        let profile = templateNavController(unSelectedImage: UIImage(systemName: "person.crop.circle")!, selectedImage: UIImage(systemName: "person.crop.circle.fill")!, viewController: ProfileController())
         
         viewControllers = [feed, search, post, notification, profile]
+        
+        tabBar.tintColor = .black
+    }
+    
+    private func templateNavController(unSelectedImage: UIImage, selectedImage: UIImage, viewController: UIViewController) -> UINavigationController{
+        let nav = UINavigationController(rootViewController: viewController)
+        nav.tabBarItem.image = unSelectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        
+        return nav
     }
 }
