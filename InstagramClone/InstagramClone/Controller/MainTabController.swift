3fc0aab81e8313cc069edf5eb77f24390ed0ec55
Ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2024-10-17 22:57:19 +0700

    initial commit

---
+++ InstagramClone/InstagramClone/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramClone
+//
+//  Created by Mochamad Ikhsan Nurdiansyah on 17/10/24.
+//
+
+import Foundation
+import UIKit
+
+class MainTabController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.green
+    }
+}
---
+++ InstagramClone/InstagramCloneTests/InstagramCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneTests.swift
+//  InstagramCloneTests
+//
+//  Created by Mochamad Ikhsan Nurdiansyah on 17/10/24.
+//
+
+import Testing
+@testable import InstagramClone
+
+struct InstagramCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
