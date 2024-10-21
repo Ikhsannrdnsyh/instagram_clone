91d9e61721a6ad747ed3e6648bcafb35da07eac6
Ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2024-10-21 21:31:53 +0700

    create search user cell

--- InstagramClone/InstagramClone/Controller/SearchController.swift
+++ InstagramClone/InstagramClone/Controller/SearchController.swift
@@ -8,11 +8,33 @@
 import Foundation
 import UIKit
 
-class SearchController: UIViewController{
+
+let userCellId = "UserCell"
+
+class SearchController: UITableViewController {
     //MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.systemBlue
+        configureUI()
+    }
+    
+    //MARK: config UI
+    private func configureUI(){
+        view.backgroundColor = UIColor.white
+        tableView.register(UserCell.self, forCellReuseIdentifier: userCellId)
+    }
+    
+}
+
+extension SearchController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: userCellId, for: indexPath)
+        cell.backgroundColor = .darkGray
+        return cell
     }
 }
