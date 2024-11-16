c54f8564110e099c5b47af18514c34dc5ceb27c6
Ikhsannrdnsyh <ikhsannurdiansyah78@gmail.com>
2024-11-17 02:53:53 +0700

    create notification cell on notif controller

--- InstagramClone/InstagramClone/Controller/NotificationController.swift
+++ InstagramClone/InstagramClone/Controller/NotificationController.swift
@@ -8,11 +8,46 @@
 import Foundation
 import UIKit
 
-class NotificationController: UIViewController{
+private let reuseIdentifier = "NotificationCell"
+
+class NotificationController: UITableViewController {
+    //MARK: Properties
+    
     //MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
         
-        view.backgroundColor = UIColor.systemYellow
+        
+    }
+    
+    //MARK: Config UI
+    private func configureUI(){
+        view.backgroundColor = .white
+        navigationItem.title = "Notifications"
+        
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+}
+
+//MARK: UITableViewDataSource
+extension NotificationController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
+        return cell
+    }
+}
+
+
+//MARK: UITAbleViewDelegate
+extension NotificationController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("DEBUG: Select row \(indexPath.row)")
     }
 }
