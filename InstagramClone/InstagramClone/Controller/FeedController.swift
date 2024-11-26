a41c33ae2fbcb4330ffdd05477acbfdb9f3ef70d
Ikhsannrdnsyh <90541904+Ikhsannrdnsyh@users.noreply.github.com>
2024-11-26 21:26:11 +0700

    Feature: notification (#7)
    * bug fix: add tap on username feed
    
    * create notification cell on notif controller
    
    * create notification model
    
    * implement add notification service
    
    * implement fetch notifications from firebase
    
    * implement viewmodel notifications
    
    * implement on tap row

--- InstagramClone/InstagramClone/Controller/FeedController.swift
+++ InstagramClone/InstagramClone/Controller/FeedController.swift
@@ -117,12 +117,22 @@ extension FeedController: UICollectionViewDelegateFlowLayout {
 }
 
 extension FeedController: FeedCellDelegate {
+    func call(_ cell: FeedCell, showProfileFor uid: String) {
+        UserService.shared.fetchUser(withUid: uid) { user in
+            let controller = ProfileController(user: user)
+            self.navigationController?.pushViewController(controller, animated: true)
+        }
+    }
+    
     func call(_ cell: FeedCell, showCommentsFor post: Post) {
         let controller = CommentController(post: post)
         navigationController?.pushViewController(controller, animated: true)
     }
     
     func call(_ cell: FeedCell, didLike post: Post) {
+        guard let tab = tabBarController as? MainTabController else { return }
+        guard let user = tab.user else { return }
+        
         cell.viewModel?.post.didLike.toggle()
         
         if post.didLike {
@@ -136,9 +146,10 @@ extension FeedController: FeedCellDelegate {
                 cell.likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
                 cell.likeButton.tintColor = .red
                 cell.viewModel?.post.likes = post.likes + 1
+                
+                NotificationService.shared.addNotification(toUid: post.ownerUid, fromUser: user, type: .like, post: post)
             }
         }
     }
-    
 }
 
---
+++ InstagramClone/InstagramClone/Model/Notification.swift
@@ -0,0 +1,50 @@
+//
+//  Notification.swift
+//  InstagramClone
+//
+//  Created by Mochamad Ikhsan Nurdiansyah on 17/11/24.
+//
+
+import UIKit
+import FirebaseFirestore
+
+enum NotificationType: Int {
+    case like
+    case follow
+    case comment
+    
+    var message: String{
+        switch self {
+        case .like:
+            return " liked your post."
+        case .follow:
+            return " started Following you"
+        case .comment:
+            return " commented on your post"
+        }
+    }
+}
+
+struct Notification {
+    let id: String
+    let uid: String
+    let profileImage: String?
+    let username: String?
+    let postId: String?
+    let postImage: String?
+    let type: NotificationType
+    let timestamp: Timestamp
+    var userIsFollowed: Bool
+    
+    init(dictionary: [String : Any]) {
+        self.id = dictionary["id"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.profileImage = dictionary["profileImage"] as? String ?? ""
+        self.username = dictionary["username"] as? String ?? ""
+        self.postId = dictionary["postId"] as? String ?? ""
+        self.postImage = dictionary["postImage"] as? String ?? ""
+        self.type = NotificationType(rawValue: dictionary["type"] as? Int ?? 0) ?? .like
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+        self.userIsFollowed = false
+    }
+}
